8ebbdcb7ee46c69e83df1b997031a475c9f3ef0c
Ashish <ashish74624@gmail.com>
2024-09-26 05:38:08 +0530

    Jai

--- app/Components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <section className=' w-screen h-5 grid place-content-center overflow-hidden text-white bg-black'>
-    </section>
-  )
-}
